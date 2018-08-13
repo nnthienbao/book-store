0699291e8164874975e537830f7896ae3e832e2c
Unknown <nnthienbao040@gmail.com>
2018-08-13 16:10:45 +0700

    Add book, remove book

--- BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/admin/BookAdminController.java
+++ BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/admin/BookAdminController.java
@@ -5,20 +5,61 @@
  */
 package com.mycompany.bookstorewebapp.controller.admin;
 
+import com.mycompany.bookstorethriftshare.Book;
+import com.mycompany.bookstorewebapp.client.Client;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
 public class BookAdminController {
+    
+    @Autowired
+    private Client client;
+    
     @GetMapping("/addbook")
-    public String getAddBook() {
+    public String getAddBook(Model model) {
+        model.addAttribute("newbook", new Book());
         return "admin/add-book";
     }
     
     @GetMapping("/updatebook")
-    public String getUpdateBook() {
+    public String getUpdateBook(@RequestParam("id") String id, Model model) {
+        try {
+            Book bookUpdate = client.findById(id);
+            
+        } catch (TException ex) {
+            Logger.getLogger(BookAdminController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "admin/update-book";
+    }
+    
+    @PostMapping("/addbook")
+    public String postUpdateBook(@ModelAttribute("newbook") Book newbook, Model model) {
+        try {
+            client.add(newbook);            
+        } catch (TException ex) {
+            Logger.getLogger(BookAdminController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "redirect:/admin/addbook";
+    }
+    
+    @PostMapping("/removebook")
+    public String postRemoveBook(@RequestParam("idBook") String idBook) {
+        try {
+            client.remove(idBook);
+        } catch (TException ex) {
+            Logger.getLogger(BookAdminController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "redirect:/admin";
     }
 }
--- BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/admin/HomeAdminController.java
+++ BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/admin/HomeAdminController.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.bookstorewebapp.controller.admin;
 
+import com.mycompany.bookstorewebapp.client.Client;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class HomeAdminController {
+    
+    @Autowired
+    private Client client;
+    
     @GetMapping({"", "/", "/index"})
     public String getIndex(Model model) {
+        try {
+            model.addAttribute("listBooks", client.getList());            
+        } catch (TException ex) {
+            ex.printStackTrace();
+        }
         return "admin/index";
     }
 }
