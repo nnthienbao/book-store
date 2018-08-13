2b6c54126b62fcce0483d2b3041fb51fe4e62aa5
Unknown <nnthienbao040@gmail.com>
2018-08-13 15:02:55 +0700

    Trang chi tiet sach

--- BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/BookController.java
+++ BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/BookController.java
@@ -5,15 +5,32 @@
  */
 package com.mycompany.bookstorewebapp.controller;
 
+import com.mycompany.bookstorethriftshare.Book;
+import com.mycompany.bookstorewebapp.client.Client;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
+    
+    @Autowired
+    private Client client;
+    
     @RequestMapping("/book")
-    public String getBook() {
+    public String getBook(@RequestParam(value = "id") String id, Model model) {
+        try {
+            Book book = client.findById(id); 
+            model.addAttribute("book", book);
+        } catch (TException ex) {
+            Logger.getLogger(BookController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "book";
     }
 }
