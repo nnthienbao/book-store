191e49909b0fba5a3cd3d38b20a4fdccae178e2e
Unknown <nnthienbao040@gmail.com>
2018-08-15 13:47:48 +0700

    fix authen layout

--- BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/AuthenController.java
+++ BookStoreWebApp/src/main/java/com/mycompany/bookstorewebapp/controller/AuthenController.java
@@ -13,37 +13,24 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.thrift.TException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class AuthenController {
-    
-    @Autowired
-    private ClientFactory clientFactory;
+public class AuthenController {   
     
     @RequestMapping("/login")
     public String getLogin() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/admin";
+        }
         return "login";
-    }
-    
-//    @PostMapping("/login")
-//    public String postLogin(@RequestParam("username") String username, 
-//            @RequestParam("password") String password,
-//            HttpServletRequest request) {
-//        try {
-//            String token = clientFactory.getUserClient().authenticate(username, password);
-//            DecodedJWT decode = JWTManager.decode(token);
-//            String role = decode.getClaim("role").asString();
-//            request.getSession().setAttribute("username", username);
-//            request.getSession().setAttribute("role", role);
-//            request.getSession().setAttribute("token", token);
-//            return "redirect:/admin";
-//        } catch (TException ex) {
-//            Logger.getLogger(AuthenController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return "redirect:/login";
-//    }
+    }   
 }
